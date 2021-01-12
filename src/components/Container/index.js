f92ad575e42022b66b9585ad03a4dc5fda5ca7c2
Ciervos <55422681+Ciervos@users.noreply.github.com>
2021-01-11 22:05:31 -0300

    ubicación de layout

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -8,9 +8,9 @@ function Container(props) {
 console.log(props)
 
   return (
- <><div className="cont-sideinfo"><p>"Side Info"</p></div>
- <div className="cont-topmenu"><p>"Top Menu"</p></div>
- <div className="cont-midcont">{props.children}</div>
+ <><div className="cont-left"><div className="cont-sideinfo"><p>"Side Info"</p></div>
+ <div className="cont-right"><div className="cont-topmenu"><p>"Top Menu"</p></div>
+ <div className="cont-midcont">{props.children}</div></div></div>
  <div className="cont-player"><p>"Player"</p></div>
  </>
   );
