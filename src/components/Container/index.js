4ff5b00e5eaf6c0bf9c01008971c16558a5fb462
Ciervos <55422681+Ciervos@users.noreply.github.com>
2021-02-16 19:08:40 -0300

    retomando aplicando variables de estilo

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -9,8 +9,10 @@ console.log(props)
 
   return (
  <><div className="cont-left"><div className="cont-sideinfo"><p>"Side Info"</p></div>
- <div className="cont-right"><div className="cont-topmenu"><p>"Top Menu"</p></div>
- <div className="cont-midcont">{props.children}</div></div></div>
+ <div className="cont-right">
+   <div className="cont-topmenu"><p>"Top Menu"</p></div>
+ <div className="cont-midcont">{props.children}</div>
+ </div></div>
  <div className="cont-player"><p>"Player"</p></div>
  </>
   );
