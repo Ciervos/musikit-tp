f987f99f0ee4db525cdc506bdb7f6c8477bad921
Ciervos <55422681+Ciervos@users.noreply.github.com>
2021-03-11 17:56:28 -0300

    menu superior

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Topmenu from "../Topmenu/index";
 import './style.scss'
 
 
@@ -9,7 +10,7 @@ function Container(props) {
   return (
  <><div className="cont-left"><div className="cont-sideinfo"><p>"Side Info"</p></div>
  <div className="cont-right">
-   <div className="cont-topmenu"><p>"Top Menu"</p></div>
+   <div className="cont-topmenu"><Topmenu/></div>
  <div className="cont-midcont">{props.children}</div>
  </div></div>
  <div className="cont-player"><p>"Player"</p></div>
---
+++ src/components/Topmenu/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './style.scss';
+
+function Topmenu() {
+  return (<><span className="topmenu-items">DESTACADOS</span> 
+           <span className="topmenu-items">PODCASTS</span> 
+           <span className="topmenu-items">LISTAS</span> 
+           <span className="topmenu-items">GENEROS Y ESTADOS DE ANIMO</span>
+           <span className="topmenu-items">NUEVOS LANZAMIENTOS</span>
+           <span className="topmenu-items">DESCUBRIR</span>
+           </>);
+}
+
+export default Topmenu;
