34a0a3a266a101867ad1c59dbadb48ac58c7b850
Ciervos <55422681+Ciervos@users.noreply.github.com>
2021-04-22 20:16:17 -0300

    estructura player

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -6,10 +6,19 @@ import {useSelector} from 'react-redux';
 
 
 function Player() {
+  const song = useSelector((state)=>state.playertrack);
 
-
-  return (<><span>Player time :)</span>
-           </>);
+  return (<div className="player-cont">
+  <div className="player-songinfo">
+    <img className="player-songimg" src={song.img}/>
+    <div className="player-nameauthor">
+      <span className="songname">{song.name}</span>
+      <span className="songauthor">{song.author}</span>
+    </div>
+  </div>
+  <div className="player-player">simbolos+duración</div>
+  <div className="player-volumen">icono depositivos+volumen</div>
+           </div>);
 }
 
 export default Player;
